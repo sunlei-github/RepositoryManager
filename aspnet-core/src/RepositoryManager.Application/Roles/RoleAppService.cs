9f8a206501bc8d6692795b567c82bba3475c2684
sl <sunlei@boya-triz.com>
2021-01-02 14:51:05 +0800

    去掉abp的默权限 暂时去掉jwt的token验证

--- aspnet-core/src/RepositoryManager.Application/Roles/RoleAppService.cs
+++ aspnet-core/src/RepositoryManager.Application/Roles/RoleAppService.cs
@@ -17,7 +17,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace RepositoryManager.Roles
 {
-    [AbpAuthorize(PermissionNames.Pages_Roles)]
+    //[AbpAuthorize(PermissionNames.Pages_Roles)]
     public class RoleAppService : AsyncCrudAppService<Role, RoleDto, int, PagedRoleResultRequestDto, CreateRoleDto, RoleDto>, IRoleAppService
     {
         private readonly RoleManager _roleManager;
