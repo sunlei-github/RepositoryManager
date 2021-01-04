5db5d655079f2e8210bf4ec1981137177f8d7d56
万成 付 <2832846911@qq.com>
2021-01-05 00:05:33 +0800

    仓库库存表的CRUD

---
+++ aspnet-core/src/RepositoryManager.Application/Business/Warehouse/AppService/WarehouseInventoryAppService.cs
@@ -0,0 +1,63 @@
+﻿using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using Abp.UI;
+using RepositoryManager.Business.Warehouse.Dto;
+using RepositoryManager.Business.Warehouse.IAppService;
+using RepositoryManager.Entities.WarehouseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RepositoryManager.Business.Warehouse.AppService
+{
+    public class WarehouseInventoryAppService : ApplicationService, IWarehouseInventoryAppService
+    {
+
+        public readonly IRepository<DbWarehouseInventory> _warehouseInventoryRepository;
+
+        public WarehouseInventoryAppService(IRepository<DbWarehouseInventory> warehouseInventoryRepository) 
+        {
+            _warehouseInventoryRepository = warehouseInventoryRepository;
+        }
+        public void CreateWarehouseInventory(CreateWarehouseInventoryInput input)
+        {
+            var WarehouseInventoryEntity = ObjectMapper.Map<DbWarehouseInventory>(input);
+
+            _warehouseInventoryRepository.Insert(WarehouseInventoryEntity);
+        }
+
+        public void DeleteWarehouseInventory(DeleteWarehouseInventoryInput input)
+        {
+            var WarehouseInventoryEntity = _warehouseInventoryRepository.FirstOrDefault(input.Id);
+            if (WarehouseInventoryEntity==null)
+            {
+                throw new UserFriendlyException($"找不到此数据{input.Id}");
+            }
+
+            _warehouseInventoryRepository.Delete(c => c.Id==input.Id);
+        }
+
+        public void EditWarehouseInventory(EditWarehouseInventoryInput input)
+        {
+            var WarehouseInventoryEntity = _warehouseInventoryRepository.FirstOrDefault(input.Id);
+            if (WarehouseInventoryEntity==null)
+            {
+                throw new UserFriendlyException($"找不到此数据{input.Id}");
+            }
+
+            _warehouseInventoryRepository.Update(WarehouseInventoryEntity);
+        }
+
+        public SearchWarehouseInventoryOutput SearchWarehouseInventory(SearchWarehouseInventoryInput input)
+        {
+            var WarehouseInventoryEntity = _warehouseInventoryRepository.FirstOrDefault(input.Id);
+            if (WarehouseInventoryEntity==null)
+            {
+                throw new UserFriendlyException($"找不到此数据{input.Id}");
+            }
+            return ObjectMapper.Map<SearchWarehouseInventoryOutput>(WarehouseInventoryEntity);
+
+        }
+    }
+}
---
+++ aspnet-core/src/RepositoryManager.Application/Business/Warehouse/Dto/WarehouseInventoryDto.cs
@@ -0,0 +1,65 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using RepositoryManager.Entities.WarehouseEntities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryManager.Business.Warehouse.Dto
+{
+    [AutoMap(typeof(DbWarehouseInventory))]
+   public class WarehouseInventoryDto : EntityDto
+    {
+
+        /// <summary>
+        /// 仓库Id
+        /// </summary>
+        public int WarehouseId { set; get; }
+
+        /// <summary>
+        /// 产品Id
+        /// </summary>
+        public int ProductId { set; get; }
+
+        /// <summary>
+        /// 剩余数量
+        /// </summary>
+        public long Balance { set; get; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remarks { set; get; }
+    }
+
+    /// <summary>
+    /// 新增仓库库存的Dto
+    /// </summary>
+    public class CreateWarehouseInventoryInput : WarehouseInventoryDto
+    { }
+
+    /// <summary>
+    /// 编辑仓库库存的Dto
+    /// </summary>
+    public class EditWarehouseInventoryInput : WarehouseInventoryDto
+    { }
+
+    /// <summary>
+    /// 删除仓库库存的Dto
+    /// </summary>
+    public class DeleteWarehouseInventoryInput : EntityDto
+    { }
+
+    /// <summary>
+    /// 输入ID查询查询整个仓库库存
+    /// </summary>
+    public class SearchWarehouseInventoryInput : EntityDto
+    { }
+
+    /// <summary>
+    /// 根据输入的ID返回整个仓库库存的信息
+    /// </summary>
+    public class SearchWarehouseInventoryOutput : WarehouseInventoryDto
+    { }
+
+}
